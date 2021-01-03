688860b2f49794b5b030a1d01eaf8bcfe9925264
Warren (laptop) <stowewa@gmail.com>
2021-01-03 09:53:06 -0500

    corrected mock data IDs

--- ProgrammingStudies/MockData/Data.cs
+++ ProgrammingStudies/MockData/Data.cs
@@ -15,14 +15,17 @@ namespace ProgrammingStudies.MockData
             { new Person("Willa", "Stowe", 1, "Pittsburgh", "PA", 4)  },
             { new Person("Helga", "Alzner", 62, "Columbus", "OH", 5) },
             { new Person("Andy", "Yurasek", 50, "Canal Winchester", "OH", 6) },
-            { new Person("Nerraw", "Ewots", 35, "Pittsburgh", "PA", 1) },
-            { new Person("Nna", "Ewots", 34, "Pittsburgh", "PA", 2)  },
-            { new Person("Edylc", "Ewots", 68, "Cincinnati", "OH", 3)  },
-            { new Person("Alliw", "Ewots", 1, "Pittsburgh", "PA", 4)  },
-            { new Person("Agleh", "Alzner", 62, "Columbus", "OH", 5) },
-            { new Person("Ydna", "Yurasek", 50, "Canal Winchester", "OH", 6) },
-            { new Person("Sara", "Stowe", 39, "Dayton", "OH", 7) },
-            { new Person("Alex", "Stowe", 16, "Dayton", "OH", 8) }
+            { new Person("Nerraw", "Ewots", 35, "Pittsburgh", "PA", 7) },
+            { new Person("Nna", "Ewots", 34, "Pittsburgh", "PA", 8)  },
+            { new Person("Edylc", "Ewots", 68, "Cincinnati", "OH", 9)  },
+            { new Person("Alliw", "Ewots", 1, "Pittsburgh", "PA", 10)  },
+            { new Person("Agleh", "Alzner", 62, "Columbus", "OH", 11) },
+            { new Person("Ydna", "Yurasek", 50, "Canal Winchester", "OH", 12) },
+            { new Person("Sara", "Stowe", 39, "Dayton", "OH", 13) },
+            { new Person("Alex", "Stowe", 16, "Dayton", "OH", 14) },
+            { new Person("Jared", "Cox", 39, "Imperial", "PA", 15) },
+            { new Person("Andrea", "Cox", 38, "Imperial", "PA", 16) },
+            { new Person("Brandon", "Hinerman", 36, "Pataskala", "OH", 17) }
         };
 
 
